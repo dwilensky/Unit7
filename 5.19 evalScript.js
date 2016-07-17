d16c8c00e4b90ea77f5c88a5f253cae41f29ea1a
Dovi <d_wilensky@hotmail.com>
2016-07-16 20:04:39 -0700

    Add files via upload

---
+++ 5.19 evalScript.js
@@ -0,0 +1,14 @@
+/* 
+ * Exercise 5.19 Evaluate Script	
+ * Evaluate what happens when this script is run
+ * Dovi Wilensky, CS81, 16Jul2016              
+ */
+
+ // alert can only be declared as a variable within a function body
+alert("Hello");	// confused JS Engine thinks that alert is a function, 
+				// attempts to run the function with parameter "World" 
+				// returns error [alert is not a function]	
+var alert = 2;	// declares alert as a variable initialized to '2'
+alert("World");	// confused JS Engine thinks that alert is a function, 
+				// attempts to run the function with parameter "World" 
+				// returns error [alert is not a function]	
