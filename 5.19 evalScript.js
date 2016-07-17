af46ed5bc954fec9dba9f438a2cd60a9dfb021bd
Dovi <d_wilensky@hotmail.com>
2016-07-16 22:07:04 -0700

    Update 5.19 evalScript.js

--- 5.19 evalScript.js
+++ 5.19 evalScript.js
@@ -5,9 +5,7 @@
  */
 
  // alert can only be declared as a variable within a function body
-alert("Hello");	// confused JS Engine thinks that alert is a function, 
-				// attempts to run the function with parameter "World" 
-				// returns error [alert is not a function]	
+alert("Hello");	// alerts "Hello"	
 var alert = 2;	// declares alert as a variable initialized to '2'
 alert("World");	// confused JS Engine thinks that alert is a function, 
 				// attempts to run the function with parameter "World" 
